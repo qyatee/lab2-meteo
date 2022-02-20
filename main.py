c679f6acf19543c10078374ba7bdfa45dedbb2e2
Mihail <2big4u7@gmail.com>
2022-02-20 17:20:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests, secret
+
+city="Moscow,RU"
+appid=secret.appid
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Город:", city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+print("Скорость ветра", data['wind']['speed'])
+print("Видимость", data['visibility'])
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("\nПрогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <",
+          '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
+          i['weather'][0]['description'], "> \r\nСкорость ветра <", i['wind']['speed'],
+          "> \r\nВидимость <", i['visibility'], ">")
+    print("____________________________")
